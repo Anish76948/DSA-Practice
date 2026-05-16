e69f9490e15a453691a1d845a8a5ddc842878f75
Anish A Maurya <sbbex005anishm@gmail.com>
2026-05-16 18:23:58 +0530

    Add files via upload

---
+++ Arrays/move_zeroes.py
@@ -0,0 +1,15 @@
+# Move Zeroes
+
+def move_zeroes(arr):
+    j = 0
+    
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[i], arr[j] = arr[j], arr[i]
+            j += 1
+    
+    return arr
+
+# Example
+arr = [0,1,0,3,12]
+print(move_zeroes(arr))
